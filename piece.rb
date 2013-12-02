1d35d3c83f305888a88b52b0774e0d5511ef00f7
App Academy Student <student@appacademy.io>
2013-12-02 10:50:35 -0800

    Add advance (vector method) and maybe_promote (king checker)

--- piece.rb
+++ piece.rb
@@ -4,19 +4,25 @@ class Piece
   def self.move_diffs
     move_diffs = [[advance, -1],[advance, 1]]
     if self.promoted
-      move_diffs.concat([retreat, -1],[retreat, 1])
+      move_diffs.concat([advance * -1, -1],[advance * -1, 1])
     end
     move_diffs
-  end
-
-  def self.maybe_promote
-
   end
 
   def initialize(pos, color)
     self.pos, self.color = pos, color
     self.promoted = false
   end
+
+  def advance
+    self.color == :white ? -1 : 1
+  end
+
+  def self.maybe_promote
+    promote_row = self.color == :white ? 0 : 7
+    @promoted = true if self.pos[0] == promote_row
+  end
+
 
   def perform_slide
 
