bdccf6570a21d871d1e63b2ab6ea2be6f598b3ef
App Academy Student <student@appacademy.io>
2013-12-02 11:03:41 -0800

    Add bracket methods for Board

--- board.rb
+++ board.rb
@@ -1,11 +1,21 @@
 class Board
 
-  attr_accessor :grid
-
   def initialize(fill_board = true)
-    self.grid = Array.new(8) { Array.new(8) }
+    @grid = Array.new(8) { Array.new(8) }
     set_pieces if fill_board
     nil
+  end
+
+  def [](pos)
+    y, x = pos
+    @grid[y][x]
+  end
+
+  protected
+
+  def []=(pos, piece)
+    y, x = pos
+    @grid[y][x] = piece
   end
 
 end
