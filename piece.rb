076d8d5c74989cc384e19ebdd456fd6dc9bc4df6
App Academy Student <student@appacademy.io>
2013-12-02 10:39:30 -0800

    Add blank methods for Piece

--- piece.rb
+++ piece.rb
@@ -1,3 +1,26 @@
 class Piece
+  attr_accessor :pos, :color, :promoted
+
+  def self.move_diffs
+
+  end
+
+  def self.maybe_promote
+
+  end
+
+  def initialize(pos, color)
+    self.pos, self.color = pos, color
+    self.promoted = false
+  end
+
+  def perform_slide
+
+  end
+
+  def perform_jump
+
+  end
+
 
 end
